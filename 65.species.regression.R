943c082ffe8a30dd42ddfc8b881025521d23086e
lhamon31 <lhamon21@gmail.com>
2017-05-21 14:51:47 -0400

    updated old git version of file with recent local version. includes region subset code

--- 65.species.regression.R
+++ 65.species.regression.R
@@ -1,8 +1,7 @@
 #####LOADING SPATIAL DATA, CHECKING MAP PROJECTIONS
 
-#make some crazy edits
-
-setwd("~/temp")
+##x-values- temperature
+setwd("~/temp/1990")
 files=list.files()
 library(raster)
 
@@ -13,16 +12,31 @@ jan90<-raster(files[8])
 
 #read in county boundary file
 library(rgdal)
-state=readOGR("C:/Users/lhamon/Documents/boundarydata","cb_2014_us_state_500k")
+state=readOGR("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/boundarydata","cb_2014_us_state_500k")
 NC<-state[state$NAME=="North Carolina",]
 
 #check map projections
 NC_geog = spTransform(NC, crs(jan90))
+plot(jan90)
+plot(NC_geog, add=T)
+proj4string(NC_geog)
+
+#crop data and plot
+jan90NC = crop(jan90, NC_geog)
+plot(jan90NC)
+plot(NC_geog,add=T)
 
 #####READING IN ALL APPROPRIATE TEMP FILES AND FINDING NCTEMPMEAN
 
+mean.earlydate<-read.csv("C:/Users/lhamon/Documents/Documents/Biology/BIOL 395H/mean.arrivalmonth.11.9.2015.csv")
+mean.earlydate<-read.csv("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/earlymonth.2016.csv") 
+#it's calculating it for each species for each year for each county
+#i guess my game plan had been to average all this biz over all the counties
+#is that okay though?
+#i have the data for doing it just for the state. relevant code is under papilio_glaucus
 species = unique(mean.earlydate$species)
-years = 1993:2014
+years = 1990:2016
+
 
 #for loop to read in all the files
 output = data.frame(county = character(),
@@ -31,7 +45,7 @@ output = data.frame(county = character(),
                     temp = numeric())
 
 # Specify months of climate data to get
-numMonths = 8 # You can change this here if you decide to use a longer or shorter window
+numMonths = 12 # You can change this here if you decide to use a longer or shorter window
 
 for (s in species) { # add a species loop to pull out species-specific arrival month
   arrivMonth = mean.earlydate$arrivalMonth[mean.earlydate$species == s]
@@ -51,8 +65,8 @@ for (s in species) { # add a species loop to pull out species-specific arrival m
   # Get temperature data across years for the 8-month period up to and including
   # the arrival month for species s
   for(y in years){
-    filenames.y<- paste("C:/Users/lhamon/Documents/temp/", y, "/PRISM_tmean_stable_4kmM2_", y, monthsText1, "_bil.bil", sep="")
-    filenames.prevy <-filenames<- paste("C:/Users/lhamon/Documents/temp/", y-1, "/PRISM_tmean_stable_4kmM2_", y-1, monthsText2, "_bil.bil", sep="")
+    filenames.y<- paste("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/temp data/", y, "/PRISM_tmean_stable_4kmM2_", y, monthsText1, "_bil.bil", sep="")
+    filenames.prevy <-filenames<- paste("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/temp data/", y-1, "/PRISM_tmean_stable_4kmM2_", y-1, monthsText2, "_bil.bil", sep="")
     filenames = c(filenames.y, filenames.prevy)
     temp_allmonths<-stack(filenames)
     tmean = calc(temp_allmonths, mean)
@@ -65,19 +79,69 @@ for (s in species) { # add a species loop to pull out species-specific arrival m
 # The output dataframe has 4 columns: the county name, species name, year, and mean temperature
 # for that 8-month window for that year.
 
-write.csv(output,file="species.year.tempmean.10.13.2015.csv")
+#save output from for loop
+write.csv(output, file="C:/Users/lhamo/Documents/Biology/butterfly paper 2016/tempmean.12.months.2016.csv")
+
+#merge with province labels
+labels <-read.csv("C:/Users/lhamon/Dropbox/NC butterfly project/NCbutterflies.65species.June2015.csv")
+labels<-labels[c("county","province")]
+#note: the above labels are lowercase, and include counties that are further subdivided by region
+#these counties are: burke, harnett, mcdowell, moore, polk, richmond, rutherford, wilkes
+#for now, assigned a singular region based on entity region map. 
+#the following province labels are the ones that have been cleaned up.
+labels<-read.csv("C:/Users/lhamon/Documents/Documents/Biology/BIOL 692H/fixing.province.names.2.24.2016.csv")
+dat<-merge(output,labels, by.x=c("county"),by.y=c("county"), all.x = T, all.y = T)
+
+#aggregate data by region
+#mean the county temp values FOR each species, FOR each year, FOR each province
+aggdat <-aggregate(dat, by=list(dat$species,dat$year,dat$province), FUN=mean, na.rm=TRUE)
+mountain.group<-subset(aggdat,aggdat$Group.3=="M")
+piedmont.group<-subset(aggdat,aggdat$Group.3=="P")
+coast.group<-subset(aggdat,aggdat$Group.3=="C")
+
+#clean the above files
+mountains<-mountain.group[c("Group.1","Group.3","year","temp")]
+colnames(mountains)<-c("species","province","year","temp")
+
+piedmont<-piedmont.group[c("Group.1","Group.3","year","temp")]
+colnames(piedmont)<-c("species","province","year","temp")
+
+coast<-coast.group[c("Group.1","Group.3","year","temp")]
+colnames(coast)<-c("species","province","year","temp")
+
 
 #####ADD JULIAN DATE AND MERGE
 
-MyData<-read.csv("C:/Users/lhamon/Dropbox/NC butterfly project/ncbutterfly.tenpercent.summary.9.29.2015.csv")
+#load earlydat data. Note that the province labels here are according to their original subdivisions, so that should be changed
+earlydate<-read.csv("C:/Users/lhamon/Documents/Documents/Biology/BIOL 692H/data/10.percent.by.province.2.17.2016.csv")
+earlydate<-read.csv("C:/Users/lhamo/Documents/Biology/butterfly paper 2016")
 
-mydat<-subset(MyData[,2:4])
+#remove whole-state temperature values
+earlydate<-earlydate[c("species","year","province","earlydate")]
 
-tempjulian = merge(output, mydat, by.x = c('species','year'), by.y =c('species','year'), all.x = T, all.y = T)
+#subset by province
+M.earlydate<-subset(earlydate,province=="M")
+P.earlydate<-subset(earlydate,province=="P")
+C.earlydate<-subset(earlydate,province=="C")
 
-earlydate<-tempjulian$earlydate
-temp<-tempjulian$temp #check you haven't overrided some other object here
+#merge earlydate and tempdat for each region
+M.tempjulian = merge(mountains, M.earlydate, by.x = c('species','year'), by.y =c('species','year'), all.x = T, all.y = T)
+M.tempjulian<-na.omit(M.tempjulian)
+M.tempjulian<-M.tempjulian[c("species","year","province.x","temp","earlydate")]
+colnames(M.tempjulian)<-c("species","year","province","temp","earlydate")
 
-#plots temperature das x-axis and first flight as y
-lm(formula = earlydate ~ temp, data = tempjulian, subset = species)
-plot(earlydate ~ temp, groups=species, data=tempjulian)
+P.tempjulian = merge(piedmont, P.earlydate, by.x = c('species','year'), by.y =c('species','year'), all.x = T, all.y = T)
+P.tempjulian<-na.omit(P.tempjulian)
+P.tempjulian<-P.tempjulian[c("species","year","province.x","temp","earlydate")]
+colnames(P.tempjulian)<-c("species","year","province","temp","earlydate")
+
+C.tempjulian = merge(coast, C.earlydate, by.x = c('species','year'), by.y =c('species','year'), all.x = T, all.y = T)
+C.tempjulian<-na.omit(C.tempjulian)
+C.tempjulian<-C.tempjulian[c("species","year","province.x","temp","earlydate")]
+colnames(C.tempjulian)<-c("species","year","province","temp","earlydate")
+
+#save as csv for each region
+write.csv(M.tempjulian,file="C:/Users/lhamon/Documents/Documents/Biology/BIOL 692H/data/mountain.fulldat.2.24.2016.csv")
+write.csv(P.tempjulian,file="C:/Users/lhamon/Documents/Documents/Biology/BIOL 692H/data/piedmont.fulldat.2.24.2016.csv")
+write.csv(C.tempjulian,file="C:/Users/lhamon/Documents/Documents/Biology/BIOL 692H/data/coast.fulldat.2.24.2016.csv")
+
