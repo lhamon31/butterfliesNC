b2cd5d4623b78f306b37f51d303f793578dbc4e1
lhamon31 <lhamon21@gmail.com>
2017-07-24 12:08:44 -0400

    updated notes with region specific results, made code to possible remove species with limited data

--- histogram.slope.R
+++ histogram.slope.R
@@ -5,19 +5,24 @@ setwd("~/Biology/butterfly paper 2016")
 
 #loading data. Any fulldat file may be substituted
 #full state
-alldat<-read.csv("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/data/fulldat.12months.NC.2016.csv")
-alldat<- alldat[ -c(1)]
+alldat<-read.csv("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/data/fulldat.4months.NC.2016.csv")
 
 #regional
-alldat<-read.csv("C:/Users/lhamon/Documents/Documents/Biology/BIOL 692H/data/coast.fulldat.2.24.2016.csv")
+alldat<-read.csv("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/data/mountain.fulldat.csv")
+alldat<-read.csv("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/data/piedmont.fulldat.csv")
+alldat<-read.csv("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/data/coast.fulldat.csv")
 
-#how to modify the regional data to eliminate species that aren't the same b/w region
-#mountains
-alldat<-alldat[-c(161:184,280:303,636:658,846),]
-#piedmont
-alldat<-alldat[-c(183:192,310:333,680:698,929:938),]
-#coast
-alldat<-alldat[-c(292:293,644:645,874:896), ]
+#Modify the regional data to eliminate species that aren't the same b/w region
+#the coast is missing Meadow Fritillary, which tracks
+#run this to clean out these spp
+alldat<-alldat[ which( ! alldat$species %in% "Boloria bellona") , ]
+#the mountains has very little data for Wallengrenia otho, Polities vibex, Callophrys gryneus (<6 years)
+#maybe run this in addition
+alldat<-alldat[ which( ! alldat$species %in% "Wallengrenia otho") , ]
+alldat<-alldat[ which( ! alldat$species %in% "Polites vibex") , ]
+alldat<-alldat[ which( ! alldat$species %in% "Callophrys gryneus") , ]
+
+
 
 ##############################################################################
 
@@ -47,8 +52,8 @@ species<-unique(alldat$species)
 
 for (s in species) {
   df<-alldat[alldat$species==s,]
-  lm.sub<-lm(df$earlydate~df$temp)
-  slope<-summary(lm.sub)$coefficients[2,1]
+  lm.sub<-lm(df$earlydate~df$year)
+  slope<-summary(lm.sub)$coefficients[2,1] 
   rsquared<-summary(lm.sub)$r.squared
   pvalue<-summary(lm.sub)$coefficients[2,4]
   tempoutput<- data.frame(species = s, slope=slope, rsquared = rsquared,
@@ -56,7 +61,11 @@ for (s in species) {
   output<-rbind(output,tempoutput)
 }
 
-
+#note that when you run the mountain data you get an error that says the slope subscript is out of bounds
+  #why this occurs is unclear. It still generates a list.
+  #The data is structured the same as with the piedmont and coast
+  #running species individually yields the same slope as generated in the output for loop
+  #just something to monitor
 
 mean(output$slope)
 ok1<-subset(output, output$pvalue<0.05)
@@ -74,32 +83,33 @@ histogram1<-ggplot(output, aes(x=slope))+
   xlab("Slope (days/degrees celsius)")
 #aes=aesthetics
 
-#slope proportions whole state, 4 months
-  #year(since earlydate is same despite temp window, year results are the same)
-    #40/56 negative, 14 significant, mean slope=-0.5056561
-  #temp
-    #38/56 negative, 9 significant, mean slope=-3.003061
-#slope proportions whole state, 6 months
-  #temp
-    #32/56 negative, 6 significant, mean slope=-1.657351
-#slope proportions whole state, 8 months
-  #temp
-    #36/56 negative, 5 significant, mean slope=-2.029041
-#slope proportions whole state, 12 months 
-  #temp 
-    #41/56 negative, 6 significant, mean slope=-3.4333367
+#whole state
+    #4 months
+      #year(since the used earlydate is same despite temp window, year results are the same)
+        #40/56 negative, 14 significant, mean slope=-0.5056561
+      #temp
+        #38/56 negative, 9 significant, mean slope=-3.003061
+    #6 months
+      #temp
+        #32/56 negative, 6 significant, mean slope=-1.657351
+    #8 months
+      #temp
+        #36/56 negative, 5 significant, mean slope=-2.029041
+    #12 months 
+      #temp 
+        #41/56 negative, 6 significant, mean slope=-3.4333367
     
-    
+  
 
 #slope proportions regional
   #year
-    #piedmont 40/61 negative mean= -0.3091232
-    #mountain 51/61 negative mean=-1.455317
-    #coast mean=38/61 negative mean=-0.5056615
-  #temperature
-    #piedmont 49/61 negative mean=-5.92268
-    #mountain 45/61 negative mean=-7.293122
-    #coast 43/61 negative, mean=-7.406275
+    #mountain 27/55 negative, 10 significant mean=-1.158833
+    #piedmont 32/55 negative, 6 significant mean=-0.04517956
+    #coast 41/55 negative, 9 significant, mean=-0.6402211
+ #temperature
+    #mountain 28/55 negative, 5 significant mean=-8.639224
+    #piedmont 39/55 negative, 6 significant mean=-4.537344
+    #coast 39/55 negative, 4 significant, mean=-5.852375
 
 
 #binomial test to test whether number of negative slopes is greater 
