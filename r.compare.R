191ae8b14c02b468a55d2e245ff818e165ddbe05
lhamon31 <lhamon21@gmail.com>
2017-08-20 23:46:13 -0400

    found difference between rs, included binary determination of which is better predictor

--- r.compare.R
+++ r.compare.R
@@ -1,8 +1,3 @@
-#comparing year and temp predictor variables using r (pearson's correlation coefficient)
-#first create a table with the slopes and r-squareds for both temp and year for each of the 56 spp
-#calculate the correlation coefficient
-# it is (the sign of the slope)* the square root of r-squared
-#add r back into the table
 #proportion of spp for whom year/temp is a better fit (r closer to +/-1)
 
 #load summary data table 
@@ -13,3 +8,15 @@ alldat<-read.csv("C:/Users/lhamo/Documents/Biology/butterfly paper 2016/data/yea
 alldat$T.r<-sign(alldat$Tslope)*sqrt(alldat$Trsquared)
 #year
 alldat$Y.r<-sign(alldat$Yslope)*sqrt(alldat$Yrsquared)
+
+#difference between T.r and Y.r
+alldat$rdifference<-(alldat$T.r)-(alldat$Y.r)
+
+#is temp a better predictor (y/n)?
+alldat$temp.better<-ifelse(abs(alldat$T.r)>abs(alldat$Y.r),"Y","N")
+
+table(alldat$temp.better)
+
+#year is a better predictor in 36/56 instances
+#potentially something other than temp an important predictor of arrival
+  #photoperiod, traits, etc. 
